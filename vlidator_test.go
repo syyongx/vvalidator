187b97e5aac03c0856bbb058350305cd2b7e8773
syyongx <syyong.x@gmail.com>
2018-06-09 14:32:08 +0800

    Add test

--- vlidator_test.go
+++ vlidator_test.go
@@ -3,14 +3,45 @@ package vvalidator
 import (
 	"testing"
 	"fmt"
+	"reflect"
 )
 
 func TestIs(t *testing.T) {
-	param := ""
-	str := ValidateString(param, "param", 1, 5, "yyy")
-	fmt.Println(str)
-	res1 := IsIPv4("111.111.111.255")
-	fmt.Println(res1)
-	res2 := IsIPv6("2001:db8::68")
-	fmt.Println(res2)
+	ipv4 := IsIPv4("8.8.8.8")
+	equal(t, true, ipv4)
+	ipv6 := IsIPv6("2001:db8::68")
+	equal(t, true, ipv6)
+}
+
+func TestValidate(t *testing.T) {
+	defer func() {
+		r := recover()
+		if err, ok := r.(*Error); ok {
+			fmt.Println(err.Message)
+		}
+	}()
+
+	params := map[string]string{
+		"uid":      "123",
+		"nickname": "fengmoti",
+		"height":   "1.5",
+	}
+
+	uid1 := ValidateInt(params, "uid", 0, 200, 10)
+	equal(t, 123, uid1)
+	uid2 := ValidateInt(params, "uids", 0, 200, 10)
+	equal(t, 10, uid2)
+	uid3 := ValidateInt(params, "uids", 0, 10, 10)
+	equal(t, 10, uid3)
+	uid4 := ValidateInt(params, "uid", 0, 10)
+	fmt.Println(uid4)
+	str := ValidateString(params, "nickname", 0, 20, "default")
+	equal(t, "fengmoti", str)
+}
+
+// Expected to be equal.
+func equal(t *testing.T, expected, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", expected, reflect.TypeOf(expected), actual, reflect.TypeOf(actual))
+	}
 }
