fa5873c24d41c8316d92f764b3b8d52578bd5551
syyongx <syyong.x@gmail.com>
2018-06-09 13:50:44 +0800

    Fix some bug

--- vlidator.go
+++ vlidator.go
@@ -111,6 +111,11 @@ func IsEmail(str string) bool {
 	return regexp.MustCompile(PatternEmail).MatchString(str)
 }
 
+// Validate 32 bit integer
+func ValidateInt(data interface{}, key string, min, max int, def ... int) int {
+	return ValidateInte(data, key, min, max, DefaultCode, "", def...)
+}
+
 // Validate 32 bit integer with custom error info.
 func ValidateInte(data interface{}, key string, min, max int, code int, note string, def ... int) int {
 	var defVal interface{}
@@ -119,17 +124,23 @@ func ValidateInte(data interface{}, key string, min, max int, code int, note str
 		defVal = def[0]
 	}
 	val := checkExist(data, key, code, note, defVal)
-	if value, ok := val.(string); ok {
+
+	switch val.(type) {
+	case int:
+		return val.(int)
+	case string:
+		value := val.(string)
 		if !IsInt(value) {
 			if ldef == 0 {
-				panic("xxx")
+				panic(NewError(key+" must be an integer", code, note))
 			}
 			return def[0]
 		}
+
 		v, err := strconv.Atoi(value)
 		if err != nil {
 			if ldef == 0 {
-				panic("xxx")
+				panic(NewError(key+" must be an integer", code, note))
 			}
 			return def[0]
 		}
@@ -146,14 +157,14 @@ func ValidateInte(data interface{}, key string, min, max int, code int, note str
 			return def[0]
 		}
 		return v
-	} else {
-		return def[0]
+	default:
+		panic("type invalid, must be string or int")
 	}
 }
 
-// Validate 32 bit integer
-func ValidateInt(data interface{}, key string, min, max int, def ... int) int {
-	return ValidateInte(data, key, min, max, DefaultCode, "", def...)
+// Validate 64 bit integer.
+func ValidateInt64(data interface{}, key string, min, max int64, def ... int64) int64 {
+	return ValidateInt64e(data, key, min, max, DefaultCode, "", def...)
 }
 
 // Validate 64 bit integer with custom error info.
@@ -164,13 +175,19 @@ func ValidateInt64e(data interface{}, key string, min, max int64, code int, note
 		defVal = def[0]
 	}
 	val := checkExist(data, key, code, note, defVal)
-	if value, ok := val.(string); ok {
+
+	switch val.(type) {
+	case int64:
+		return val.(int64)
+	case string:
+		value := val.(string)
 		if !IsInt(value) {
 			if ldef == 0 {
 				panic(NewError(key+" must be a valid interger", code, note))
 			}
 			return def[0]
 		}
+
 		v, err := strconv.ParseInt(value, 10, 64)
 		if err != nil {
 			if ldef == 0 {
@@ -191,14 +208,13 @@ func ValidateInt64e(data interface{}, key string, min, max int64, code int, note
 			return def[0]
 		}
 		return v
-	} else {
-		return def[0]
+	default:
+		panic("type invalid, must be string or int64")
 	}
 }
 
-// Validate 64 bit integer.
-func ValidateInt64(data interface{}, key string, min, max int64, def ... int64) int64 {
-	return ValidateInt64e(data, key, min, max, DefaultCode, "", def...)
+func ValidateFloat(data interface{}, key string, min, max float64, code int, note string, def ... float64) float64 {
+	return ValidateFloate(data, key, min, max, DefaultCode, "", def...)
 }
 
 //  Validate 64 bit float with custom error info.
@@ -209,37 +225,46 @@ func ValidateFloate(data interface{}, key string, min, max float64, code int, no
 		defVal = def[0]
 	}
 	val := checkExist(data, key, code, note, defVal)
-	if !IsFloat(val.(string)) {
-		if ldef == 0 {
-			panic(NewError(key+" must be a valid float", code, note))
+	switch val.(type) {
+	case float64:
+		return val.(float64)
+	case string:
+		value := val.(string)
+		if !IsFloat(value) {
+			if ldef == 0 {
+				panic(NewError(key+" must be a valid float", code, note))
+			}
+			return def[0]
 		}
-		return def[0]
-	}
-	value, err := strconv.ParseFloat(val.(string), 2)
-	if err != nil {
-		if ldef == 0 {
-			panic(NewError(key+" must be a valid interger", code, note))
-		}
-		return def[0]
-	}
-	if min != -1 && value < min {
-		if ldef == 0 {
-			panic(NewError(key+" is too small (minimum is "+strconv.FormatFloat(min, 'f', -1, 64)+")", code, note))
-		}
-		return def[0]
-	}
-	if max != -1 && value > max {
-		if ldef == 0 {
-			panic(NewError(key+" is too big (maximum is "+strconv.FormatFloat(max, 'f', -1, 64)+")", code, note))
-		}
-		return def[0]
-	}
 
-	return value
+		v, err := strconv.ParseFloat(val.(string), 64)
+		if err != nil {
+			if ldef == 0 {
+				panic(NewError(key+" must be a valid interger", code, note))
+			}
+			return def[0]
+		}
+		if min != -1 && v < min {
+			if ldef == 0 {
+				panic(NewError(key+" is too small (minimum is "+strconv.FormatFloat(min, 'f', -1, 64)+")", code, note))
+			}
+			return def[0]
+		}
+		if max != -1 && v > max {
+			if ldef == 0 {
+				panic(NewError(key+" is too big (maximum is "+strconv.FormatFloat(max, 'f', -1, 64)+")", code, note))
+			}
+			return def[0]
+		}
+		return v
+	default:
+		panic("type invalid, must be string or float64")
+	}
 }
 
-func ValidateFloat(data interface{}, key string, min, max float64, code int, note string, def ... float64) float64 {
-	return ValidateFloate(data, key, min, max, DefaultCode, "", def...)
+// Validate enum int.
+func ValidateEnumInt(data interface{}, key string, validValues []int, code int, note string, def ... int) int {
+	return ValidateEnumInte(data, key, validValues, DefaultCode, "", def...)
 }
 
 // Validate enum int with custom error info.
@@ -250,12 +275,12 @@ func ValidateEnumInte(data interface{}, key string, validValues []int, code int,
 			return val
 		}
 	}
-	panic(NewError(key+" is not valid", code, note))
+	panic(NewError(key+" is invalid", code, note))
 }
 
-// Validate enum int.
-func ValidateEnumInt(data interface{}, key string, validValues []int, code int, note string, def ... int) int {
-	return ValidateEnumInte(data, key, validValues, DefaultCode, "", def...)
+// Validate enum int64
+func ValidateEnumInt64(data interface{}, key string, validValues []int64, code int, note string, def ... int64) int64 {
+	return ValidateEnumInt64e(data, key, validValues, DefaultCode, "", def...)
 }
 
 // Validate enum int64
@@ -266,12 +291,12 @@ func ValidateEnumInt64e(data interface{}, key string, validValues []int64, code 
 			return val
 		}
 	}
-	panic(NewError(key+" is not valid", code, note))
+	panic(NewError(key+" is invalid", code, note))
 }
 
-// Validate enum int64
-func ValidateEnumInt64(data interface{}, key string, validValues []int64, code int, note string, def ... int64) int64 {
-	return ValidateEnumInt64e(data, key, validValues, DefaultCode, "", def...)
+// Validate string
+func ValidateEnumString(data interface{}, key string, validValues []string, def ... string) string {
+	return ValidateEnumStringe(data, key, validValues, DefaultCode, "", def...)
 }
 
 // Validate enum string with custom error info.
@@ -282,12 +307,7 @@ func ValidateEnumStringe(data interface{}, key string, validValues []string, cod
 			return val
 		}
 	}
-	panic(key + " is not valid")
-}
-
-// Validate string
-func ValidateEnumString(data interface{}, key string, validValues []string, def ... string) string {
-	return ValidateEnumStringe(data, key, validValues, DefaultCode, "", def...)
+	panic(NewError(key+" is invalid", code, note))
 }
 
 // Validate string with custom error info.
@@ -298,23 +318,27 @@ func ValidateStringe(data interface{}, key string, min, max int, code int, note 
 	}
 	val := checkExist(data, key, code, note, defVal)
 
-	if value, ok := val.(string); ok {
-		length := utf8.RuneCountInString(value)
-		if min != -1 && length < min {
-			panic(NewError(key+"is too short (minimum is "+strconv.Itoa(min)+" characters)", code, note))
-		}
-		if max != -1 && length > max {
-			panic(NewError(key+"is too long (maximum is "+strconv.Itoa(max)+" characters)", code, note))
-		}
-		return value
-	} else {
+	length := utf8.RuneCountInString(val.(string))
+	if len(def) > 0 && length == 0 {
 		return def[0]
 	}
+	if min != -1 && length < min {
+		panic(NewError(key+"is too short (minimum is "+strconv.Itoa(min)+" characters)", code, note))
+	}
+	if max != -1 && length > max {
+		panic(NewError(key+"is too long (maximum is "+strconv.Itoa(max)+" characters)", code, note))
+	}
+	return val.(string)
 }
 
 // Validate string.
 func ValidateString(data interface{}, key string, min, max int, def ... string) string {
 	return ValidateStringe(data, key, min, max, DefaultCode, "", def...)
+}
+
+// Validate slice.
+func ValidateSlice(data interface{}, key, sep string, min, max int, def ... string) []string {
+	return ValidateSlicee(data, key, sep, min, max, DefaultCode, "", def...)
 }
 
 // Validate slice with custom error info.
@@ -325,49 +349,34 @@ func ValidateSlicee(data interface{}, key, sep string, min, max int, code int, n
 	}
 	val := checkExist(data, key, code, note, defVal)
 
-	if value, ok := val.(string); ok {
-		vals := strings.Split(value, sep)
-		length := len(vals)
-		if min != -1 && length < min {
-			panic(key + "is too short (minimum is " + strconv.Itoa(min) + " elements)")
-		}
-		if max != -1 && length > max {
-			panic(NewError(key+"is too long (maximum is "+strconv.Itoa(max)+" elements)", code, note))
-		}
-		return vals
-	} else {
-		panic(NewError("xxx", code, note))
+	vals := strings.Split(val.(string), sep)
+	length := len(vals)
+	if min != -1 && length < min {
+		panic(NewError(key+" is too short (minimum is "+strconv.Itoa(min)+" elements)", code, note))
 	}
-}
-
-// Validate slice.
-func ValidateSlice(data interface{}, key, sep string, min, max int, def ... string) []string {
-	return ValidateSlicee(data, key, sep, min, max, DefaultCode, "", def...)
+	if max != -1 && length > max {
+		panic(NewError(key+" is too long (maximum is "+strconv.Itoa(max)+" elements)", code, note))
+	}
+	return vals
 }
 
 // Chekc exist
 func checkExist(data interface{}, key string, code int, note string, def interface{}) interface{} {
 	var val string
-
 	switch data.(type) {
 	case string:
 		val = data.(string)
 	case map[string]string:
-		if val, ok := data.(map[string]string)[key]; ok {
-			if val == "" {
-				if def == nil {
-					panic(NewError(key+"is required", code, note))
-				} else {
-					return def
-				}
-			}
-		} else if def == nil {
-			panic(NewError(key+" is required", code, note))
+		if value, ok := data.(map[string]string)[key]; ok {
+			val = value
 		} else {
+			if def == nil {
+				panic(NewError(key+" is required", code, note))
+			}
 			return def
 		}
 	default:
-		panic("data type nonsupport")
+		panic(NewError("data type invalid, must be string or map[string]string", code, note))
 	}
 
 	if val == "" {
